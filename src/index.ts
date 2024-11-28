04a12a59c7f312b14b906dc6aaa45cba8ff46227
German G <german95guerrero@hotmail.com>
2024-11-28 08:12:36 -0500

    railway setup3

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,12 @@ const main = () => {
     const app = express()
     app.use(router);
     const PORT = process.env.PORT || 3002;
+
+    app.get("/api", (req, res) => {
+        res.send({ msg: "Welcome to Gym REPORT micro services 📚" });
+    });
+
+
     app.listen(PORT, () => {
         console.log(`Microservices running at port ${PORT}`);
     })
